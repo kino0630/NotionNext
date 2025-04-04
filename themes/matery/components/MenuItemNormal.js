9b5369aa090853ac2664790cfa87a091c837d0b7
kino0630 <kino0630233@gmail.com>
2025-04-04 12:35:22 +0800

    Update MenuItemNormal.js

--- themes/matery/components/MenuItemNormal.js
+++ themes/matery/components/MenuItemNormal.js
@@ -15,7 +15,7 @@ export const MenuItemNormal = props => {
       className={
         'py-2 px-5 duration-300 text-base justify-between hover:bg-gray-700 hover:text-white hover:shadow-lg cursor-pointer font-light flex flex-nowrap items-center ' +
         (selected
-          ? 'bg-indigo-500 text-white '
+          ? 'bg-darkslategray-500 text-white '
           : ' text-black dark:text-white ')
       }>
       <div className='my-auto items-center justify-between flex '>
